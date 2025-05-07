edc99a37714600d018ce18084370386f85d2dfd9
NPaiffer <nicpaiffer@hotmail.com>
2025-05-07 12:21:35 -0300

    feat: atualizacao integral do projeto

---
+++ ClinicaAPI.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicaAPI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- Models/AuthRequest.cs
+++
@@ -1,6 +0,0 @@
-public class AuthRequest
-{
-    public string Email { get; set; }
-    public string Password { get; set; }
-
-}
